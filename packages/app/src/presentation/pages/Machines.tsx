33435d415173dd604fd05bf9577ed07d2ba23dac
DanielCabiscol <danielcabiscol@gmail.com>
2022-07-07 13:56:53 +0200

    chore: improve routing

--- packages/app/src/presentation/pages/Machines.tsx
+++ packages/app/src/presentation/pages/Machines.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-export const Machines = () => (
-  <div>
-    <h3>MACHINES</h3>
-  </div>
-);
+import {useHistory} from 'react-router-dom';
+export const Machines = () => {
+  const history = useHistory();
+  const handleClick = () => history.push('/products');
+  return (
+    <div>
+      <h3>MACHINES</h3>
+      <button onClick={handleClick}>Go To products</button>
+    </div>
+  );
+};
--- packages/app/src/presentation/pages/Products.tsx
+++ packages/app/src/presentation/pages/Products.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
-export const Products = () => (
-  <div>
-    <h3>PRODUCTS</h3>
-  </div>
-);
+export const Products = () => {
+  const history = useHistory();
+  const handleClick = () => history.push('/machines');
+  return (
+    <div>
+      <h3>PRODUCTS</h3>
+      <button onClick={handleClick}>Go To machines</button>
+    </div>
+  );
+};
