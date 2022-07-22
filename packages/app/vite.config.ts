980ea76529d98a7601ed7edb2730a43644909527
Joan Bruguera <jbruguera@uxland.cat>
2022-07-22 13:21:26 +0200

    chore: add docker-compose file + totem v2

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  ...(process.env.VITE_HOST ? {
+    server: {
+      host: process.env.VITE_HOST,
+    }
+  } : {}),
   plugins: [react()]
 })
