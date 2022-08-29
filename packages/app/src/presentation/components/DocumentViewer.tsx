82570a262f4abe9b3ddd336b1016106d10d31c85
Albert Mosella <mosella.albert@gmail.com>
2022-08-29 17:12:49 +0200

    chore: pdf display library

--- packages/app/src/presentation/components/DocumentViewer.tsx
+++ packages/app/src/presentation/components/DocumentViewer.tsx
@@ -1,3 +1,6 @@
+import {useState} from 'react';
+import {Document, Page} from 'react-pdf';
+import {mockPdf} from '../mockFiles/mockFiles';
 interface Props {
   setShowDocument?: (value: boolean) => void;
 }
@@ -6,10 +9,23 @@ export const DocumentViewer = (props: Props): JSX.Element => {
   const handleCloseDocument = () => {
     props.setShowDocument && props.setShowDocument(false);
   };
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({numPages}) {
+    setNumPages(numPages);
+  }
   return (
     <>
       <div>Document Viewer</div>
       <div onClick={handleCloseDocument}>X</div>
+      <Document file="./dummy.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <Document file={mockPdf.attach} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
     </>
   );
 };
