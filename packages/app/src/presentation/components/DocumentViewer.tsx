49d42b9cc06971c727fafda0d292a45aec1c87a7
Albert Mosella <mosella.albert@gmail.com>
2022-08-30 13:33:47 +0200

    chore: functional pdf and img dispalyer

--- packages/app/src/presentation/components/DocumentViewer.tsx
+++ packages/app/src/presentation/components/DocumentViewer.tsx
@@ -1,6 +1,8 @@
-import {useState} from 'react';
-import {Document, Page} from 'react-pdf';
-import {mockPdf} from '../mockFiles/mockFiles';
+import { useState } from "react";
+// import {Document, Page} from 'react-pdf';
+import { mockPdf, mockPdf2, mockImg } from "../mockFiles/mockFiles";
+import FilePreViewer from "react-file-previewer";
+import "./DocumentViewer.scss";
 interface Props {
   setShowDocument?: (value: boolean) => void;
 }
@@ -13,19 +15,33 @@ export const DocumentViewer = (props: Props): JSX.Element => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({numPages}) {
+  function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
   return (
-    <>
+    <div className="viewer">
       <div>Document Viewer</div>
       <div onClick={handleCloseDocument}>X</div>
-      <Document file="./dummy.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      {/* <Document file="./dummy.pdf" onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
       </Document>
       <Document file={mockPdf.attach} onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
-      </Document>
-    </>
+      </Document> */}
+      <FilePreViewer
+        file={{
+          data: mockImg.attach,
+          mimeType: "image/jpeg",
+          name: "image.jpg",
+        }}
+      />
+      <FilePreViewer
+        file={{
+          data: mockPdf2.attach,
+          mimeType: "application/pdf",
+          name: "docu.pdf",
+        }}
+      />
+    </div>
   );
 };
